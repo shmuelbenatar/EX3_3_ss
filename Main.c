0be0748dcae6b3c17bb19ab186e81b31b0922188
shmuelbenatar <shmuelbe.at@gmail.com>
2024-02-20 14:32:20 +0200

    added main switches

--- Main.c
+++ Main.c
@@ -1,1 +1,64 @@
-Main.c
+
+#include <stdio.h>
+
+int main()
+{
+    int userChoice;
+
+    printf("Enter a number between 0 and 12: ");
+    scanf("%d", &userChoice);
+    do
+    {
+
+        switch (userChoice)
+        {
+        case 1:
+            printf("You chose option 1\n");
+            
+            break;
+        case 2:
+            printf("You chose option 2\n");
+            break;
+        case 3:
+            printf("You chose option 3\n");
+            break;
+        case 4:
+            printf("You chose option 4\n");
+            break;
+        case 5:
+            printf("You chose option 5\n");
+            break;
+        case 6:
+            printf("You chose option 6\n");
+            break;
+        case 7:
+            printf("You chose option 7\n");
+            break;
+        case 8:
+            printf("You chose option 8\n");
+            break;
+        case 9:
+            printf("You chose option 9\n");
+            break;
+        case 10:
+            printf("You chose option 10\n");
+            break;
+        case 11:
+            printf("You chose option 11\n");
+            break;
+        case 12:
+            printf("You chose option 12\n");
+            break;
+        case 13:
+            printf("You chose option 13\n");
+            break;
+        case 0:
+            printf("You chose option 13\n");
+            break;
+        default:
+            break;
+        }
+
+    } while (userChoice != 0 && userChoice != EOF); // Continue the loop until the user chooses to exit
+    return 0;
+}
